4e2168232102b0c8cf533033779853e9cffc4c3e
Jean-Charles <jean-charles.simonnet@isen.yncrea.fr>
2022-08-07 11:05:52 +0200

    Init alog dev

--- fold/codility/SolutionA.java
+++ fold/codility/SolutionA.java
@@ -11,38 +11,49 @@ import static java.util.stream.IntStream.rangeClosed;
 
 public class SolutionA {
 
+
+    public int solution(int A) {
+        var res= 0;
+        return res;
+    }
+
     public static  void main(String args[]) {
-        int n = 4000000;
+        int n = 4;
         var streamInt= IntStream.concat( rangeClosed(1, 3),  iterate(5, i->i+3).limit(n));
         var ranged = IntStream.concat(iterate(5, i->i+2).limit(n),streamInt).boxed().collect(Collectors.toList());
         Collections.shuffle(ranged);
         Object[] aaa =ranged.stream().toArray();
-        Object[][][] A= new Object[][][]{
-                {new Object[]{6},new Object[]{3}},
-                {aaa,new Object[]{4}},
-                {new Object[]{1},new Object[]{2}}
+        Object[][] A= new Object[][]{
+                {new Object[]{"mesage"},new Object[] { 3, 8, 9, 7, 6 }, 3,  new Object[]{  9, 7, 6,  3, 8 }},
+                {new Object[]{""},new Object[] { 3, 8, 9, 7, 6 }, 3,  new Object[]{  9, 7, 6,  3, 8 }
+                }
         };
         executeTest(A);
     }
 
-    private static void executeTest(Object[][][] A) {
-        for (Object[][] a: A) {
+    private static void executeTest(Object[][] A) {
+        for (Object[] a: A) {
             long reference=System.nanoTime();
-            var b = Arrays.stream(a[0])
-                    .mapToInt(x-> (int) x)
-                    .toArray();
+            var arrayInt1 = toIntAt((Object[]) a[1]);
+            int sec = (int) a[2];
             out.println("\nInput:" + Arrays.deepToString(a));
 
-            out.println("\nOutput:"+ new MinMissingPositiveIntegerInAnArraySonar()
-                    .solution(b) + " ,Expected:" + Arrays.toString(a[1])
+            out.println("\nOutput:"
+                    + //Arrays.toString(
+                            new SolutionA().solution(sec)
+                    + " \nExpect:"
+                    + Arrays.toString((Object[]) a[3])
             );
             long finishm=System.nanoTime();
             out.println("Time " + ( (double)(finishm-reference)/1000000 )+ "ms");  //in seconds
         }
     }
-    public int solution(int A) {
-        var res= 0;
-        return res;
+
+    private static int[] toIntAt(Object[] a) {
+        return Arrays.stream(a)
+                .mapToInt(x -> (int) x)
+                .toArray();
     }
+
 
 }
--- fold/codility/lessons02/CyclicRotation.java
+++ fold/codility/lessons02/CyclicRotation.java
@@ -1,4 +1,60 @@
 package fold.codility.lessons02;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.System.out;
+import static java.util.stream.IntStream.iterate;
+import static java.util.stream.IntStream.rangeClosed;
+
 public class CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        int [] rotatedA = new int[A.length];
+
+        for(int i=0; i<A.length; i++) {
+            //rotated index needs to "wrap" around end of array
+            int rotatedIndex = (i + K) % A.length;
+
+            rotatedA[rotatedIndex] = A[i];
+        }
+        return rotatedA;
+    }
+    public static  void main(String args[]) {
+        int n = 4;
+        var streamInt= IntStream.concat( rangeClosed(1, 3),  iterate(5, i->i+3).limit(n));
+        var ranged = IntStream.concat(iterate(5, i->i+2).limit(n),streamInt).boxed().collect(Collectors.toList());
+        Collections.shuffle(ranged);
+        Object[] aaa =ranged.stream().toArray();
+        Object[][] A= new Object[][]{
+                {new Object[]{"mesage"},new Object[] { 3, 8, 9, 7, 6 }, 3,  new Object[]{  9, 7, 6,  3, 8 }},
+                {new Object[]{""},new Object[] { 3, 8, 9, 7, 6 }, 3,  new Object[]{  9, 7, 6,  3, 8 }
+                }
+        };
+        executeTest(A);
+    }
+
+    private static void executeTest(Object[][] A) {
+        for (Object[] a: A) {
+            long reference=System.nanoTime();
+            var arrayInt1 = toIntAt((Object[]) a[1]);
+            int sec = (int) a[2];
+            out.println("\nInput:" + Arrays.deepToString(a));
+
+            out.println("\nOutput:"+ Arrays.toString(new CyclicRotation()
+                    .solution(arrayInt1, sec)) + " \nExpect:" + Arrays.toString((Object[]) a[3])
+            );
+            long finishm=System.nanoTime();
+            out.println("Time " + ( (double)(finishm-reference)/1000000 )+ "ms");  //in seconds
+        }
+    }
+
+    private static int[] toIntAt(Object[] a) {
+        return Arrays.stream(a)
+                .mapToInt(x -> (int) x)
+                .toArray();
+    }
+
+
 }
